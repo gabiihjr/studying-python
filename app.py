f557f3eb98b46ad81369c67869f7908557bca5a8
Gabriela Hermenegildo <gabriela@cybera.io>
2025-05-28 15:29:37 -0300

    fundamentals of python - beginning

--- app.py
+++ app.py
@@ -8,5 +8,14 @@ y = 2
 unit_price = 3
 
 # Fundamentals of Python
+# Python is a case sensitive language!
+
 students_count = 1000
+rating = 4.99  # float
+is_published = True  # boolean
+course = "Python Programming"  # string
 print(students_count)
+
+message = """
+Testing long message!
+"""
