0e864cc398042060f020b15ef3645ef16838911d
Gabriela Hermenegildo <gabriela@cybera.io>
2025-05-28 17:53:32 -0300

    studying while loops

--- basic-python/loops.py
+++ basic-python/loops.py
@@ -39,3 +39,28 @@ else:
 for x in range(5):
     for y in range(3):
         print(f"({x}, {y})")
+
+
+# While loops
+number = 100
+while number > 0:
+    print(number)
+    number //= 2
+
+command = ""
+while command != "quit" and command != "QUIT":
+    command = input(">")
+    print("ECHO", command)
+
+command = ""
+while command.tolower() != "quit":
+    command = input(">")
+    print("ECHO", command)
+
+
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count += 1
+        print(number)
+print(f"We have {count} even numbers")
