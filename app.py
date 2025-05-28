5a5d559bc4ef69aa95f1770564bc00bf4848c55d
Gabriela Hermenegildo <gabriela@cybera.io>
2025-05-28 15:25:13 -0300

    first look into python

---
+++ app.py
@@ -0,0 +1,12 @@
+﻿print("Hello World")
+print("*" * 10)
+# Python Enhancement Proposals: PEPs
+# Famous PEP: PEP 8 - Style Guide for Python Code https://www.python.org/dev/peps/
+
+x = 1  # declaring a variable
+y = 2
+unit_price = 3
+
+# Fundamentals of Python
+students_count = 1000
+print(students_count)
